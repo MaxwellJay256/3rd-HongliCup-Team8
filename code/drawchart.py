9fab7333dde20cbce09a11c1ce6b5df649433fdf
MaxwellJay256 <MaxwellJay@jaylab256.onmicrosoft.com>
2024-01-16 21:24:36 +0800

    add savefig feature

--- code/drawchart.py
+++ code/drawchart.py
@@ -4,13 +4,13 @@ import matplotlib.pyplot as plt
 # 读取数据
 dataType = 'reference'
 subject = '1'
-trial = '2'
+trial = '4'
 
 fileName = f'code\\{dataType}\\{subject}\\{trial}.txt'
 df = np.loadtxt(fileName)
 
 # 折线图
-fig, axs = plt.subplots(nrows=2, ncols=1, figsize=(8, 4))
+fig, axs = plt.subplots(nrows=2, ncols=1, figsize=(10, 4))
 fig.suptitle(f'{dataType} {subject}-{trial}')
 subtitles = ['Accel', 'Gyro']
 Axes = ['X','Y','Z']
@@ -22,10 +22,12 @@ for i in range(2):
     axs[i].set_title(subtitles[i])
 plt.tight_layout()
 plt.legend()
+plt.savefig(f'code\\images\\{dataType}-{subject}-{trial}.png')
 plt.show()
 
 # 箱型图
-fig, ax = plt.subplots(figsize=(8, 6))
+fig, ax = plt.subplots(figsize=(7, 5))
 fig.suptitle(f'{dataType} {subject}-{trial}')
 ax.boxplot(df.T, labels=[f'{i+1}' for i in range(df.shape[0])])
+plt.savefig(f'code\\images\\{dataType}-{subject}-{trial}-boxplot.png')
 plt.show()
